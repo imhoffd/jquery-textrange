6085b621011c96b2736512dc90dc94f422ee0d68
Daniel Imhoff <dwieeb@gmail.com>
2012-03-14 20:34:06 -0500

    Everything appears to be working as intended

--- jquery-textrange.js
+++ jquery-textrange.js
@@ -5,6 +5,8 @@
  * (c) 2012 Daniel Imhoff <dwieeb@gmail.com> - danielimhoff.com
  */
 (function($) {
+   var browserType;
+
    var textrange = {
       /**
        * $().textrange() or $().textrange('get')
@@ -12,20 +14,10 @@
        * substring of the range.
        *
        * @param (optional) property 
-       * @return The object or 
+       * @return An object of properties including position, start, end, length, and text or a specific property.
        */
       get: function(property) {
-         var properties = {}
-
-         if('selectionStart' in this[0]) {
-            properties.position = this[0].selectionStart;
-            properties.start = this[0].selectionStart;
-            properties.end = this[0].selectionEnd;
-            properties.length = properties.end - properties.start;
-            properties.text = this.val().substring(properties.start, properties.end);
-         }
-
-         return typeof property === 'undefined' ? properties : properties[property];
+         return _textrange[browserType].get.apply(this, [property]);
       },
 
       /**
@@ -36,26 +28,143 @@
        * @param (optional) end The ending position of the selection.
        */
       set: function(start, end) {
-         if('selectionStart' in this[0]) {
-            this[0].selectionStart = start;
-            this[0].selectionEnd = end;
+         if(typeof start === 'undefined') {
+            start = 0;
          }
+         else if(start === 'all') {
+            start = 0;
+            end = 'end';
+         }
+
+         if(typeof end === 'undefined') {
+            end = start;
+         }
+         else if(end === 'end') {
+            end = this.val().length;
+         }
+
+         _textrange[browserType].set.apply(this, [start, end]);
 
          return this;
       },
 
       /**
        * $().textrange('replace')
-       * Replaces the selected text in 
+       * Replaces the selected text in the input field or textarea with text.
+       *
+       * @param text The text to replace the selection with.
        */
       replace: function(text) {
-         
+         _textrange[browserType].replace.apply(this, [text]);
 
          return this;
+      },
+
+      /**
+       * Alias for $().textrange('replace')
+       */
+      insert: function(text) {
+         return this.textrange('replace', text);
+      }
+   };
+
+   var _textrange = {
+      xul: {
+         get: function(property) {
+            var props = {
+               position: this[0].selectionStart,
+               start: this[0].selectionStart,
+               end: this[0].selectionEnd,
+               length: this[0].selectionEnd - this[0].selectionStart,
+               text: this.val().substring(this[0].selectionStart, this[0].selectionEnd)
+            };
+
+            return typeof property === 'undefined' ? props : props[property];
+         },
+
+         set: function(start, end) {
+            this[0].selectionStart = start;
+            this[0].selectionEnd = end;
+         },
+
+         replace: function(text) {
+            var start = this[0].selectionStart;
+            this.val(this.val().substring(0, this[0].selectionStart) + text + this.val().substring(this[0].selectionEnd, this.val().length));
+            this[0].selectionStart = start;
+            this[0].selectionEnd = start + text.length;
+         }
+      },
+
+      msie: {
+         get: function(property) {
+            this[0].focus();
+
+            var range = document.selection.createRange();
+
+            if(typeof range === 'undefined') {
+               return {
+                  position: 0,
+                  start: 0,
+                  end: this[0].val().length,
+                  length: this[0].val().length,
+                  text: this.val()
+               };
+            }
+
+            var rangetext = this[0].createTextRange();
+            var rangetextcopy = rangetext.duplicate();
+
+            rangetext.moveToBookmark(range.getBookmark());
+            rangetextcopy.setEndPoint('EndToStart', rangetext);
+
+            return {
+               position: rangetextcopy.text.length,
+               start: rangetextcopy.text.length,
+               end: rangetextcopy.text.length + range.text.length,
+               length: range.text.length,
+               text: range.text
+            };
+         },
+
+         set: function(start, end) {
+            this[0].focus();
+
+            var range = this[0].createTextRange();
+
+            if(typeof range === 'undefined') {
+               return this;
+            }
+
+            if(typeof start !== 'undefined') {
+               range.moveStart('character', start);
+               range.collapse();
+            }
+
+            if(typeof end !== 'undefined') {
+               range.moveEnd('character', end - start);
+            }
+
+            range.select();
+         },
+
+         replace: function(text) {
+            this[0].focus();
+
+            document.selection.createRange().text = text;
+         }
       }
    };
 
    $.fn.textrange = function(method) {
+      if(typeof browserType === 'undefined') {
+         browserType = 'selectionStart' in this[0] ? 'xul' : document.selection ? 'msie' : 'unknown';
+      }
+
+      // I don't know how to support this browser. :c
+      if(browserType === 'unknown') {
+         return this;
+      }
+
       if(typeof method === 'undefined' || typeof method !== 'string') {
          return textrange.get.apply(this);
       }
